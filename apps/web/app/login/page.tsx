0d4800d98cfa0c2aaf99c0469d109253ac922bbe
lubjr <luizeduardobjr@outlook.com>
2026-01-14 21:27:54 -0300

    fix: page login

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -1,4 +1,5 @@
 import { LoginForm } from "../../components/LoginForm";
+
 export default function LoginPage() {
   return (
     <div className="bg-zinc-900 flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
