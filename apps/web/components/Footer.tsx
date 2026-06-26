7738d4f37bbd5028810cabc39f68d310293267a5
lubjr <luizeduardobjr@outlook.com>
2026-06-26 20:42:05 -0300

    fix(light-theme): isolate site to dark mode, fix toast colors and marketing layout

--- apps/web/components/Footer.tsx
+++ apps/web/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="bg-zinc-900 text-center text-sm text-zinc-400 p-4">
+    <footer className="bg-card text-center text-sm text-zinc-400 p-4">
       © 2026 Saedra – All rights reserved.
     </footer>
   );
