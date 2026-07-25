3e94406a150f94709d19fab00ad0db6bc2115471
lubjr <luizeduardobjr@outlook.com>
2026-07-24 21:25:40 -0300

    fix: update version in header

--- apps/web/components/Header.tsx
+++ apps/web/components/Header.tsx
@@ -15,7 +15,7 @@ export const Header = () => {
           >
             Saedra
           </Link>
-          <Badge variant="outline">v2.4</Badge>
+          <Badge variant="outline">v2.5</Badge>
         </div>
 
         <Menu />
